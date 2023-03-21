95afee5e7942a2515bb12bf94d14cae4e2439d86
Ads-cmu <advaiths@andrew.cmu.edu>
2023-03-21 08:53:59 -0400

    FF implemented for MNIST

---
+++ main.py
@@ -0,0 +1,54 @@
+from FFNetwork import *
+from utilities import *
+
+config={
+    'epochs':100,
+    'lr':0.001,
+    'batch_size':64,
+    'threshold':1.0,
+}
+train_loader, test_loader = get_loaders(config['batch_size'])
+wandb.login(key="95ecc4ba8c0966a365ccf643e67d08226e659d02") #API Key is in your wandb account, under settings (wandb.ai/settings)
+# Create your wandb run
+run = wandb.init(
+    project="project",
+    entity  = "automellon",
+    name = "Advaith-FF-MNIST", ## Wandb creates random run names if you skip this field
+    reinit = False, ### Allows reinitalizing runs when you re-run this cell
+    # run_id = ### Insert specific run id here if you want to resume a previous run
+    # resume = "must" ### You need this to resume previous runs, but comment out reinit = True when using this
+    config = config ### Wandb Config for your run
+)
+net = Net([784, 2000, 2000, 2000, 2000])
+
+# batch_bar = tqdm(total=config['epochs'], dynamic_ncols=True, leave=False, position=0, desc='Train', ncols=3)
+for epoch in range(config['epochs']):
+  train_err=[] 
+  train_loss = 0.0
+  for x,y in tqdm(train_loader):
+    x,y = x.cuda(),y.cuda()
+    x_pos = overlay_y_on_x(x, y) 
+    y_neg = get_y_neg(y)
+    x_neg = overlay_y_on_x(x, y_neg)
+
+    train_loss += net.train(x_pos, x_neg)
+    train_err.append(net.predict(x).eq(y).float().mean().item())
+  
+  train_e = sum(train_err)/len(train_loader)
+  train_loss /=len(train_loader)
+  print("train accuracy",train_e)
+  print("train loss",train_loss)
+  test_err=[]
+  for x_te,y_te in test_loader:
+    x_te, y_te = x_te.cuda(), y_te.cuda()
+    test_err.append(net.predict(x_te).eq(y_te).float().mean().item())
+  
+  test_e = sum(test_err)/len(test_loader)
+  # scheduler.step(test_e)
+  print("test accuracy",test_e)
+  wandb.log({"train_accuracy":train_e, 'train_loss':train_loss,'validation_accuracy': test_e})
+  # print('test error:', sum(test_loss)/len(test_loader))
+#   batch_bar.set_postfix(train_loss = "{:.04f}".format(float(train_l)),test_loss="{:.04f}".format(float(test_l)))
+#   batch_bar.update()
+# batch_bar.close()
+run.finish()
---
+++ utilities.py
@@ -0,0 +1,104 @@
+import torch
+from torchvision.datasets import MNIST, CIFAR10
+from torchvision.transforms import Compose, ToTensor, Normalize, Lambda
+from torch.utils.data import DataLoader
+
+from PIL import Image
+import matplotlib.pyplot as plt
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+def get_loaders(batch_size):
+	transform = Compose(
+        [
+            ToTensor(),
+            Normalize((0.1307,), (0.3081,)),
+            Lambda(lambda x: torch.flatten(x)),
+        ]
+    )
+	train_loader = DataLoader(
+    MNIST("./data/", train=True, download=True, transform=transform),
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=2,
+    pin_memory=True)
+
+	test_loader = DataLoader(
+    MNIST("./data/", train=False, download=True, transform=transform),
+    batch_size=batch_size,
+    shuffle=False,
+    num_workers=2,
+    pin_memory=True)
+
+    return train_loader, test_loader
+
+def overlay_y_on_x(x, y, classes=10):
+    x_ = x.clone()
+    x_[:, :classes] *= 0.0
+    x_[range(x.shape[0]), y] = x.max()
+    return x_
+
+def get_y_neg(y,num_classes=10):
+    y_neg = y.clone()
+    for idx, y_samp in enumerate(y):
+        allowed_indices = list(range(num_classes))
+        allowed_indices.remove(y_samp.item())
+        y_neg[idx] = torch.tensor(allowed_indices)[
+            torch.randint(len(allowed_indices), size=(1,))
+        ].item()
+    return y_neg.to(DEVICE)
+
+def visualise_positive():
+    r, c    = [5, 5]
+    fig, ax = plt.subplots(r, c, figsize= (15, 15))
+    k = 0
+
+    dtl = DataLoader(
+    MNIST("./data/", train=True, download=False, transform=Compose([ToTensor(),Lambda(lambda x: torch.flatten(x))])),
+    batch_size=config['batch_size'],
+    shuffle=True,
+    num_workers=2,
+    pin_memory=True)
+
+    for data in train_loader:
+        x, y = data
+    
+        for i in range(r):
+            for j in range(c):
+                mod=x[k].reshape((1,-1)) #add dim
+                mod = overlay_y_on_x(mod,y[k])
+                img = mod.reshape((28,28)).numpy()
+                ax[i, j].imshow(img)
+                ax[i, j].axis('off')
+                k+=1
+        break
+
+    del dtl
+
+def visualise_negative():
+    r, c    = [5, 5]
+    fig, ax = plt.subplots(r, c, figsize= (15, 15))
+
+    k       = 0
+
+    dtl = DataLoader(
+    MNIST("./data/", train=True, download=False, transform=Compose([ToTensor(),Lambda(lambda x: torch.flatten(x))])),
+    batch_size=config['batch_size'],
+    shuffle=True,
+    num_workers=2,
+    pin_memory=True)
+
+    for data in train_loader:
+        x, y = data
+    
+        for i in range(r):
+            for j in range(c):
+                mod=x[k].reshape((1,-1))
+                y_neg = get_y_neg(y[k].view(1,1))
+                mod = overlay_y_on_x(mod,y_neg[0])
+                img = mod.reshape((28,28)).numpy()
+                ax[i, j].imshow(img)
+                ax[i, j].axis('off')
+                k+=1
+        break
+
+    del dtl
